012e835bf897bed67599dc984c0ab166de98a7cb
giorgi168 <giorgi.mamagulashvili.1@btu.edu.ge>
2021-07-08 08:45:17 +0400

    Added Post Item UI, Dashboard Fragment UI

---
+++ app/src/main/java/com/example/you/ui/fragments/dashboard/DashboardFragment.kt
@@ -0,0 +1,21 @@
+package com.example.you.ui.fragments.dashboard
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.you.R
+import com.example.you.databinding.DashboardFragmentBinding
+import com.example.you.ui.base.BaseFragment
+
+class DashboardFragment : BaseFragment<DashboardFragmentBinding>(DashboardFragmentBinding::inflate) {
+    override fun start(inflater: LayoutInflater, viewGroup: ViewGroup?) {
+        init()
+    }
+    private fun init(){
+
+    }
+
+}
