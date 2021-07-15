440fbc837e05aefd72c532356882c0ba4850cd59
giorgi168 <giorgi.mamagulashvili.1@btu.edu.ge>
2021-07-15 22:20:55 +0400

    Added Registration

--- app/src/main/java/com/example/you/app/BaseApplication.kt
+++ app/src/main/java/com/example/you/app/BaseApplication.kt
@@ -1,8 +1,8 @@
 package com.example.you.app
 
 import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
 
-@HiltAndroidApp
+
+
 class BaseApplication:Application() {
 }
---
+++ app/src/main/java/com/example/you/models/user/UserModel.kt
@@ -0,0 +1,7 @@
+package com.example.you.models.user
+
+data class UserModel(
+    val uid:String = "",
+    val userName:String = "",
+    val status:String = "",
+)
